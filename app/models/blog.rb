06e5b217a464b70cb84775a5d79c9243dadb8008
mikan-v7y <mikan.v7y@gmail.com>
2026-05-31 21:17:15 +0900

    fix: SQLインジェクション防止のため、検索処理をプレースホルダー化

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -10,7 +10,11 @@ class Blog < ApplicationRecord
   scope :published, -> { where('secret = FALSE') }
 
   scope :search, lambda { |term|
-    where("title LIKE '%#{term}%' OR content LIKE '%#{term}%'")
+    where(
+      'title LIKE ? OR content LIKE ?',
+      "%#{term}%",
+      "%#{term}%"
+    )
   }
 
   scope :default_order, -> { order(id: :desc) }
