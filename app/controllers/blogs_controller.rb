11c3d42a0780089e251ebad830421a5258031d1a
mikan-v7y <mikan.v7y@gmail.com>
2026-06-08 21:57:12 +0900

    fix: プレミアムユーザー以外はrandom_eyecatchを許可しないよう修正

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -62,7 +62,10 @@ class BlogsController < ApplicationController
   end
 
   def blog_params
-    params.expect(blog: %i[title content secret random_eyecatch])
+    permitted = %i[title content secret]
+    permitted << :random_eyecatch if current_user.premium?
+
+    params.expect(blog: permitted)
   end
 
   def authorize_user!
