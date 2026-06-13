e01ff24ab13f446ee12b8e65dedb37ffd8348cd1
mikan-v7y <mikan.v7y@gmail.com>
2026-06-14 01:10:11 +0900

    refactor: Blog取得時の重複クエリを解消

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -3,9 +3,7 @@
 class BlogsController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index show]
 
-  before_action :set_blog, only: %i[show edit update destroy]
-
-  before_action :authorize_user!, only: %i[edit update destroy]
+  before_action :set_owned_blog, only: %i[edit update destroy]
 
   def index
     @blogs = Blog.search(params[:term]).published.default_order
@@ -47,10 +45,6 @@ class BlogsController < ApplicationController
 
   private
 
-  def set_blog
-    @blog = Blog.find(params[:id])
-  end
-
   def blog_params
     permitted = %i[title content secret]
     permitted << :random_eyecatch if current_user.premium?
@@ -58,7 +52,7 @@ class BlogsController < ApplicationController
     params.expect(blog: permitted)
   end
 
-  def authorize_user!
+  def set_owned_blog
     @blog = current_user.blogs.find(params[:id])
   end
 
