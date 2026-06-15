0ec86f173a571f3345e05cfbc9cc70aadd9078fa
mikan-v7y <mikan.v7y@gmail.com>
2026-06-15 16:50:40 +0900

    refactor: 不要な条件分岐を削除

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -10,14 +10,7 @@ class BlogsController < ApplicationController
   end
 
   def show
-    @blog =
-      if current_user
-        Blog.published
-            .or(current_user.blogs)
-            .find(params[:id])
-      else
-        Blog.published.find(params[:id])
-      end
+    @blog = Blog.published.or(current_user&.blogs || Blog.none).find(params[:id])
   end
 
   def new
